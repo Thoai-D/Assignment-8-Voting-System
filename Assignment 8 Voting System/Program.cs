3cc26112f390c7d1a9ff888cfd44a40a878b1084
Thoai Do Huy <thoaidohuy@gmail.com>
2021-01-26 15:12:50 +0700

    Add project files.

---
+++ Assignment 8 Voting System/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Assignment_8_Voting_System
+{
+    class Program
+    {
+
+        private static int i = 0;
+
+        static void Main(string[] args)
+        {
+            Vote[] votes = new Vote[100];
+
+            CastVote(votes);
+        }
+
+        static void CastVote(Vote[] votes)
+        {
+
+            bool finished = false;
+            while (finished == false)
+            {
+                Console.WriteLine("What is your name?");
+                string votername = Console.ReadLine();
+                Console.WriteLine("Submit 1 for democrats, 2 for communists, 3 for facists, press any other number for EXIT");
+                int partynumber = Console.Read();
+
+
+                if (partynumber == 1)
+                {
+                    votes[i] = new Vote(votername, "Democrats");
+                }
+                else if (partynumber == 2)
+                {
+                    votes[i] = new Vote(votername, "Communists");
+                }
+                else if (partynumber == 3)
+                {
+                    votes[i] = new Vote(votername, "Facists");
+                }
+                else
+                {
+                    finished = true;
+                }
+            }
+
+
+        }
+    }
+}
---
+++ Assignment 8 Voting System/Vote.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_8_Voting_System
+{
+    class Vote
+    {
+        private string votername;
+        private string partyname; //democrats, facists, communists
+        private DateTime timevoted;
+
+        private static double totalvotes = 0;
+
+        //constructor
+        public Vote(string votername, string partyname)
+        {
+            DateTime currenttime = DateTime.Now;
+
+            this.votername = votername;
+            this.partyname = partyname;
+
+            timevoted = currenttime;
+            totalvotes = totalvotes + 1;
+        }
+
+
+        //accessor
+        public string Getpartyvotedfor()
+        {
+            return partyname;
+        }
+
+        public DateTime Gettimevoted()
+        {
+            return timevoted;
+        }
+
+
+        public static double Gettotalvotes()
+        {
+            return totalvotes;
+        }
+
+
+
+    }
+}
